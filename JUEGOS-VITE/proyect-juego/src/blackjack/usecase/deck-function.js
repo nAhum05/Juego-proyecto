f7ff4e79d7d7327eb81a1dc935e56c3f5cc40f5c
Jahuri <24300712@uttt.edu.mx>
2025-11-11 00:11:58 -0600

    feat: agregamos una libreria para dar mejora de los comentarios dentro del proyecto.

--- JUEGOS-VITE/proyect-juego/src/blackjack/usecase/deck-function.js
+++ JUEGOS-VITE/proyect-juego/src/blackjack/usecase/deck-function.js
@@ -1,16 +1,26 @@
 
 import _ from 'underscore';
 //Esta funcion crea un nuevo deck
-export const crearDeck = (tipoDeCarta, tiposEspeciales) =>{
+
+/**
+ * 
+ * @param {array} tiposDeCarta 
+ * @param {array} tiposEspeciales 
+ * @returns {array} retorna un nuevo deck de cartas 
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) =>{
+    if(!tiposDeCarta) throw new Error('tipo de carta obligatoria como un arreglo obligatorio')
+
+
     let deck = [];
     //ciclo interactivo
     for(let i = 2; i<=10; i++){
-        for (let tipo of tipoDeCarta){
+        for (let tipo of tiposDeCarta){
             deck.push ( i + tipo);
         }
     }
 
-    for (let tipo of tipoDeCarta ){
+    for (let tipo of tiposDeCarta ){
         for (let esp of tiposEspeciales){
             deck.push (esp + tipo);
         }
