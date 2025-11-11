219703c87b872756d696ef1fc9448e70335af63b
Jahuri <24300712@uttt.edu.mx>
2025-11-10 23:38:53 -0600

    fix:  se corrigio la funcionalidad del metodo de forma correcta

--- JUEGOS-VITE/proyect-juego/src/blackjack/usecase/deck-function.js
+++ JUEGOS-VITE/proyect-juego/src/blackjack/usecase/deck-function.js
@@ -1,17 +1,17 @@
 
 import _ from 'underscore';
 //Esta funcion crea un nuevo deck
-export const crearDeck = (tipoDeCartas, tiposEspeciales) =>{
+export const crearDeck = (tipoDeCarta, tiposEspeciales) =>{
     let deck = [];
     //ciclo interactivo
     for(let i = 2; i<=10; i++){
-        for (let tipo of tipoDeCartas){
+        for (let tipo of tipoDeCarta){
             deck.push ( i + tipo);
         }
     }
 
-    for (let tipo of tipos ){
-        for (let esp of especiales){
+    for (let tipo of tipoDeCarta ){
+        for (let esp of tiposEspeciales){
             deck.push (esp + tipo);
         }
     }
